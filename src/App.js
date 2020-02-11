8ca2f034d2fa76d92fb1291799003c339da6cec0
Alex <Talha>
2020-02-10 23:32:25 -0300

    manipulando estado de um componente

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import  React  from 'react';
 class App extends React.Component{
 
   state = {
-    nome : "Alex Talha"
+    nome : ""
+  }
+
+  modificarNome = (event) => {
+      let nome = event.target.value;
+      this.setState({
+        nome: nome
+      })
   }
 
     render(){
-    return <h1>Hello {this.state.nome}</h1>
-    }
-}
 
+    return ( 
+      <React.Fragment>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>Hello {this.state.nome}</h1>
+      </React.Fragment>
+    )
+}
+}
 
 export default App;
