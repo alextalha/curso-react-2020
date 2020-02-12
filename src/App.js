5edc0bc1afb14a57f8b0f4575513c79a992f8191
Alex <Talha>
2020-02-11 23:33:39 -0300

    zerando o projeto

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import  React  from 'react';
 
-
  function App(props){
+  return(
+    <div className="App">
 
-
-  const modificarNome = event => {
-    console.log(event.target.value)
-  }
-
-  const criarCombo = () => {
-    let dados = ['dados 1','dados 2']
-    let ComboOptions = dados.map( (item) => <option>{item}</option>)
-    return (
-      <select>
-        {ComboOptions}
-      </select>
-    )
-  }
-
-
-      const MeuComboBox = () => criarCombo()
-
-    return ( 
-      <React.Fragment>
-        <input className="text-centralizado" type="text" value={props.nome} onChange={modificarNome} />
-
-        <h1>Hello {props.nome}</h1>
-        <h1>Idade {props.idade}</h1>
-        <MeuComboBox />
-      </React.Fragment>
-    )
+    </div>
+  )
 }
 
 export default App;
