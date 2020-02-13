49865a24083d3da0fab83403b21eacfa241ecda9
Alex <Talha>
2020-02-13 00:21:55 -0300

    montando a pagina de produto

--- src/views/produtos/cadastro.js
+++ src/views/produtos/cadastro.js
@@ -8,6 +8,31 @@ class CadastroProduto extends React.Component {
         <div className="card border-secondary mb-3">
              <div className="card-header">Cadastro de Produto</div>
             <div className="card-body">
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="nome"  > Nome </label>
+                            <input type="text" placeholder="Digite um nome" class="form-control" id="nome"></input>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="nome"  > Sku </label>
+                            <input type="text" placeholder="Digite um Sku" class="form-control" id="sku"></input>
+                        </div>
+                    </div>
+                
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="descricao">Descrição</label>
+                        <input type="text-area" class="form-control"></input>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
         )
